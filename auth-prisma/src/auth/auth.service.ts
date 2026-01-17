0efa04ad21c563d6d8e7b5781dd65a227bae3a15
Aashray <aashray851@gmail.com>
2026-01-17 21:15:47 +0545

    added configue in auth module

--- auth-prisma/src/auth/auth.service.ts
+++ auth-prisma/src/auth/auth.service.ts
@@ -36,10 +36,7 @@ export class AuthService {
       },
     });
 
-    // const isPasswordValid = await bcrypt.compare(password, user.password);
-    // if (!isPasswordValid) {
-    //   throw new UnauthorizedException('invalid password');
-    // }
+   
 
     //removed password from user and we kept it in "result"
     const { password: _, ...result } = user;
